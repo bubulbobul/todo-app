d6090992daf3f160663630748c3dc08967dde54e
windam <windam@live>
2019-07-19 20:11:44 +0530

    run json server

--- src/App.js
+++ src/App.js
@@ -1,26 +1,95 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import axios from 'axios'
+import uuid from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import About from './components/pages/About';
+import { V4MAPPED } from 'dns';
+
+class App extends React.Component {
+  state = {
+    todos: []
+  }
+
+  componentDidMount() {
+    // axios.get('https://jsonplaceholder.typicode.com/todos')
+    axios.get('http://localhost:4000/results?_limit=10')
+      .then(res => 
+        // // console.log(res.data)
+        this.setState({ todos: res.data })
+      )
+  }
+
+  // Toggle check box todo (completed)
+  checkedInput = (id) => {
+    // // console.log(id)
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if(todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+    })
+  }
+
+  deleteTodo = (id) => {
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res => this.setState({
+          todos: [...this.state.todos.filter(todo => todo.id !== id)]
+        }) 
+      );
+
+    // this.setState({
+    // //   todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    // });
+  }
+
+  addTodo = (title) => {
+    // // console.log(title)
+    // const newTodo = {
+    //   id:uuid.v4(),
+    //   title: title,
+    //   completed: false
+    // }
+    axios.post('https://jsonplaceholder.typicode.com/todos', {
+      id: uuid.v4(),
+      title: title,
+      completed:false
+    }).then(res => this.setState({ 
+        todos: [...this.state.todos, res.data]
+      }) 
+    );
+    // // this.setState({ todos: [...this.state.todos, newTodo]})
+  } 
+
+  render() {
+    // // console.log(this.state.todos);
+    return (
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route exact path="/" render={props =>(
+              <React.Fragment>
+                <Todos 
+                  todos={this.state.todos} 
+                  checkedInput={this.checkedInput}
+                  deleteTodo={this.deleteTodo}
+                />
+                <AddTodo addTodo={this.addTodo}/>
+              </React.Fragment>
+            )} />
+            <Route path="/about" component={About}/>
+          </div>
+        </div>
+      </Router> 
+    );
+  }
 }
 
 export default App;

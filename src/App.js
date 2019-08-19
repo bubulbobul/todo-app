693b6a8e5443cef964f05590e4132be33138361c
bubulbobul <windam@live.fr>
2019-08-19 20:34:05 +0530

    todo list unshift

--- src/App.js
+++ src/App.js
@@ -1,93 +1,102 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import axios from 'axios'
-import uuid from 'uuid';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import axios from "axios";
+import uuid from "uuid";
 
-import './App.css';
-import Header from './components/layout/Header';
-import Todos from './components/Todos';
-import AddTodo from './components/AddTodo';
-import About from './components/pages/About';
-import { V4MAPPED } from 'dns';
+import "./App.css";
+import Header from "./components/layout/Header";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+import About from "./components/pages/About";
+// import { V4MAPPED } from "dns";
 
 class App extends React.Component {
   state = {
     todos: []
-  }
+  };
 
   componentDidMount() {
-    // axios.get('https://jsonplaceholder.typicode.com/todos')
-    axios.get('http://localhost:4000/results?_limit=10')
-      .then(res => 
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      // axios.get('http://localhost:4000/results?_limit=10')
+      .then(res =>
         // // console.log(res.data)
         this.setState({ todos: res.data })
-      )
+      );
   }
 
   // Toggle check box todo (completed)
-  checkedInput = (id) => {
+  checkedInput = id => {
     // // console.log(id)
     this.setState({
       todos: this.state.todos.map(todo => {
-        if(todo.id === id) {
-          todo.completed = !todo.completed
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
         }
-        return todo
+        return todo;
       })
-    })
-  }
+    });
+  };
 
-  deleteTodo = (id) => {
-    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-      .then(res => this.setState({
-          todos: [...this.state.todos.filter(todo => todo.id !== id)]
-        }) 
-      );
+  deleteTodo = id => {
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`).then(res =>
+      this.setState({
+        todos: [...this.state.todos.filter(todo => todo.id !== id)]
+      })
+    );
 
     // this.setState({
     // //   todos: [...this.state.todos.filter(todo => todo.id !== id)]
     // });
-  }
+  };
 
-  addTodo = (title) => {
+  addTodo = title => {
     // // console.log(title)
     // const newTodo = {
     //   id:uuid.v4(),
     //   title: title,
     //   completed: false
     // }
-    axios.post('https://jsonplaceholder.typicode.com/todos', {
-      id: uuid.v4(),
-      title: title,
-      completed:false
-    }).then(res => this.setState({ 
-        todos: [...this.state.todos, res.data]
-      }) 
-    );
+    axios
+      .post("https://jsonplaceholder.typicode.com/todos", {
+        id: uuid.v4(),
+        title: title,
+        completed: false
+      })
+      .then(res =>
+        this.setState({
+          todos: [res.data, ...this.state.todos]
+        })
+      );
     // // this.setState({ todos: [...this.state.todos, newTodo]})
-  } 
+  };
 
   render() {
     // // console.log(this.state.todos);
     return (
-      <Router>
-        <div className="App">
-          <div className="container">
+      <Router basename={`${process.env.PUBLIC_URL}`}>
+        <div className='App'>
+          <div className='container'>
             <Header />
-            <Route exact path="/" render={props =>(
-              <React.Fragment>
-                <Todos 
-                  todos={this.state.todos} 
-                  checkedInput={this.checkedInput}
-                  deleteTodo={this.deleteTodo}
-                />
-                <AddTodo addTodo={this.addTodo}/>
-              </React.Fragment>
-            )} />
-            <Route path="/about" component={About}/>
+            <Route
+              exact
+              path='/'
+              render={routerProps => (
+                <React.Fragment>
+                  <AddTodo routerProps={routerProps} addTodo={this.addTodo} />
+                  <Todos
+                    routerProps={routerProps}
+                    todos={this.state.todos}
+                    checkedInput={this.checkedInput}
+                    deleteTodo={this.deleteTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path='/about' component={About} />
           </div>
         </div>
-      </Router> 
+      </Router>
     );
   }
 }
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
     <React.Fragment>
-      <h1>This is the TodoList app v1.0.0. It is part of a React crash course</h1>
+      <h1 style={{ textAlign: "center" }}>
+        This is the basic TodoList app v1.0.0
+      </h1>
     </React.Fragment>
-  )
+  );
 }

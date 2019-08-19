879c5e6716fb4e18916cac3252781bb4b7efe71e
bubulbobul <windam@live.fr>
2019-08-19 20:39:53 +0530

    Updates

---
+++ precache-manifest.b879ccccbec901551a423a999cf94474.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4ef559b9d9dec2cce1c7a8d772ee911",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "6413eb888f3de0d99587",
+    "url": "/todo-app/static/css/main.bad7ddae.chunk.css"
+  },
+  {
+    "revision": "51cde5876262f780754d",
+    "url": "/todo-app/static/js/2.c237ea68.chunk.js"
+  },
+  {
+    "revision": "6413eb888f3de0d99587",
+    "url": "/todo-app/static/js/main.9b114edd.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  }
+]);
